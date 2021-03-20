b8cee563299f5002c0b40a6d9ebe631c222ec5be
Marta Kuśpit <mkuspit@gmail.com>
2021-03-20 10:37:30 +0100

    login out

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import {environment} from '../environments/environment';
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent implements OnInit{
+export class AppComponent {
   username: string;
   password: string;
   user: User = null;
@@ -20,45 +20,45 @@ export class AppComponent implements OnInit{
 
   constructor(private httpClient: HttpClient, private router: Router) {
   }
-  @HostListener('window:beforeunload')
-  onWindowUnload(): void{
-    localStorage.removeItem('authorizationHeader');
-  }
-  ngOnInit(): void {
-    this.login();
-  }
+  // @HostListener('window:beforeunload')
+  // onWindowUnload(): void{
+  //   localStorage.removeItem('authorizationHeader');
+  // }
+  // ngOnInit(): void {
+  //   this.login();
+  // }
+  //
+  // onLoginFormSubmit(): void {
+  //   this.storeAuthorizationInLocalstorage();
+  //   this.login( () => alert('Niepoprawne dane logowania'));
+  //   this.username = '';
+  //   this.password = '';
+  // }
 
-  onLoginFormSubmit(): void {
-    this.storeAuthorizationInLocalstorage();
-    this.login( () => alert('Niepoprawne dane logowania'));
-    this.username = '';
-    this.password = '';
-  }
-
-  private storeAuthorizationInLocalstorage(): void {
-    const authorizationHeader = 'Basic ' + btoa(this.username + ':' + this.password);
-    localStorage.setItem('authorizationHeader', authorizationHeader);
-  }
-
-  private login(errorCallback?: () => void): void {
-    this.httpClient.post<User>(this.baseUrl + 'login', {})
-      .subscribe(user => {
-        this.router.navigate(['']);
-        this.user = user;
-      },
-        () => {
-        if (errorCallback){
-          errorCallback();
-        }
-        });
-  }
+  // private storeAuthorizationInLocalstorage(): void {
+  //   const authorizationHeader = 'Basic ' + btoa(this.username + ':' + this.password);
+  //   localStorage.setItem('authorizationHeader', authorizationHeader);
+  // }
+  //
+  // private login(errorCallback?: () => void): void {
+  //   this.httpClient.post<User>(this.baseUrl + 'login', {})
+  //     .subscribe(user => {
+  //       this.router.navigate(['']);
+  //       this.user = user;
+  //     },
+  //       () => {
+  //       if (errorCallback){
+  //         errorCallback();
+  //       }
+  //       });
+  // }
 
 
-  logout(): void {
-    // localStorage.removeItem('authorizationHeader');
-    this.router.navigate(['']);
-    setTimeout(() => location.reload(), 100);
-  }
+  // logout(): void {
+  //   // localStorage.removeItem('authorizationHeader');
+  //   this.router.navigate(['']);
+  //   setTimeout(() => location.reload(), 100);
+  // }
 
 }
 
