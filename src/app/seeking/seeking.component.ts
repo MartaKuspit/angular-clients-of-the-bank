4ffd0e162bf202eea6bc57bcfa548eecc7cfc252
Marta Kuśpit <mkuspit@gmail.com>
2021-03-31 18:01:37 +0200

    reload

--- src/app/seeking/seeking.component.ts
+++ src/app/seeking/seeking.component.ts
@@ -28,7 +28,7 @@ export class SeekingComponent implements OnInit{
       .subscribe(clients => {
         if (this.clients !== null) {
           this.clients = clients;
-          localStorage.clear();
+          location.reload();
         }
         if (this.clients == null) {
           alert('Nieznaleziono podanej frazy. Spróbuj jeszcze raz!');
